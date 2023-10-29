b6612a93970b0d1ca334f5bd84b596d7e3b9a598
MEZIANE reda <reda.meziane@etu.univ-paris-diderot.fr>
2023-10-29 19:59:40 +0100

    Add controller class for movie

---
+++ backend/src/main/java/com/movielib/backend/controller/MovieController.java
@@ -0,0 +1,27 @@
+package com.movielib.backend.controller;
+
+import com.movielib.backend.model.Movie;
+import com.movielib.backend.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/movie")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping
+    public List<Movie> getMovie(){
+        return movieService.getMovie();
+    }
+}
