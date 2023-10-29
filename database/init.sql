69bac9aa0f513815f1d8940a2a31b73718f61c4c
MEZIANE reda <reda.meziane@etu.univ-paris-diderot.fr>
2023-10-29 20:01:36 +0100

    Add movies_reviews table to connect movie and reviews

--- database/init.sql
+++ database/init.sql
@@ -1,22 +1,20 @@
-DROP DATABASE IF EXISTS movielib CASCADE;
 DROP TABLE IF EXISTS movies CASCADE;
 DROP TABLE IF EXISTS categories CASCADE;
 DROP TABLE IF EXISTS actors CASCADE;
 DROP TABLE IF EXISTS movies_actor CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS reviews CASCADE;
+DROP TABLE IF EXISTS movie_reviews CASCADE;
 DROP SEQUENCE IF EXISTS actors_actor_id_seq;
 DROP SEQUENCE IF EXISTS categories_category_id_seq;
 DROP SEQUENCE IF EXISTS movies_movie_id_seq;
 DROP SEQUENCE IF EXISTS reviews_review_id_seq;
-DROP SEQUENCE IF EXISTS users_user_id_seq ;
-
-CREATE DATABASE movielib;
+DROP SEQUENCE IF EXISTS users_user_id_seq;
 
 CREATE TABLE categories (
     category_id serial PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    CONSTRAINT uniqe_name UNIQUE (name)
+    CONSTRAINT unique_name UNIQUE (name)
 );
 
 CREATE TABLE movies(
@@ -24,7 +22,7 @@ CREATE TABLE movies(
     title VARCHAR(255) NOT NULL,
     category VARCHAR(255) NOT NULL REFERENCES categories(name),
     summary TEXT,
-    release_date DATE,
+    release_date SMALLINT,
     rating NUMERIC(3, 2),
     director VARCHAR(255)
 );
@@ -54,3 +52,12 @@ CREATE TABLE reviews (
     rating NUMERIC(3, 2) NOT NULL,
     comment TEXT
 );
+
+CREATE TABLE movies_reviews (
+    movie_id INT REFERENCES movies (movie_id),
+    review_id INT REFERENCES reviews (review_id),
+    PRIMARY KEY (movie_id, review_id)
+);
+
+INSERT INTO users(username, email, password)
+    VALUES ('reda', 'reda@email.com', 123);
