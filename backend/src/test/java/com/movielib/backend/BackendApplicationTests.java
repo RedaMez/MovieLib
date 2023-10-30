55fff3db18bccffacaa91fc1fe9b578ce29dcd2b
MEZIANE reda <reda.meziane@etu.univ-paris-diderot.fr>
2023-10-30 22:04:52 +0100

    Added Tests modules

--- backend/src/test/java/com/movielib/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/movielib/backend/BackendApplicationTests.java
@@ -2,8 +2,14 @@ package com.movielib.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = BackendApplicationTests.class)
+@TestPropertySource(
+        locations = "classpath:application-tests.properties"
+)
 class BackendApplicationTests {
 
     @Test
