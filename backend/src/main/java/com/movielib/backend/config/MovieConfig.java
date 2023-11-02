f6f15ef406e5c38dbf4c0084bfb17579b23ea3c0
MEZIANE reda <reda.meziane@etu.univ-paris-diderot.fr>
2023-11-03 00:10:22 +0100

    Using Spring Builder

--- backend/src/main/java/com/movielib/backend/config/MovieConfig.java
+++ backend/src/main/java/com/movielib/backend/config/MovieConfig.java
@@ -17,27 +17,41 @@ public class MovieConfig {
     @Bean
     CommandLineRunner commandLineRunner(MovieRepository repository) {
         return args -> {
-            Movie movie1 = new Movie(
-                    "Alien",
-                    new Category("SF"),
-                    "The crew of a spacecraft, Nostromo, intercept a distress signal from a planet" +
+            Movie movie1 = Movie.builder()
+                    .title("Alien")
+                    .category(Category.builder().name("SF").build())
+                    .summary(
+                            "The crew of a spacecraft, Nostromo, intercept a distress signal from a planet" +
                             " and set out to investigate it. However, to their horror, they are attacked by" +
-                            " an alien which later invades their ship.",
-                    1979,
-                    "Ridley Scott",
-                    new ArrayList<>(List.of(new Actor("Sigourney Weaver"), new Actor("Tom Skerritt"), new Actor("Veronica Cartwright")))
-            );
+                            " an alien which later invades their ship."
+                    )
+                    .releaseDate(1979)
+                    .director("Ridley Scott")
+                    .actors(
+                            new ArrayList<>(List.of(
+                                    Actor.builder().name("Sigourney Weaver").build(),
+                                    Actor.builder().name("Tom Skerritt").build(),
+                                    Actor.builder().name("Veronica Cartwright").build()
+                            ))
+                    ).build();
 
-            Movie movie2 = new Movie(
-                    "The godfather",
-                    new Category("Drama"),
-                    "Don Vito Corleone, head of a mafia family, decides to hand over his empire " +
+            Movie movie2 = Movie.builder()
+                    .title("The Godfather")
+                    .category(Category.builder().name("Drama").build())
+                    .summary(
+                            "Don Vito Corleone, head of a mafia family, decides to hand over his empire " +
                             "to his youngest son, Michael. However, his decision unintentionally puts the " +
-                            "lives of his loved ones in grave danger.",
-                    1972,
-                    "Francis Ford Coppola",
-                    new ArrayList<>(List.of(new Actor("Al Pacino"), new Actor("Marlon Brando"), new Actor("James Caan")))
-            );
+                            "lives of his loved ones in grave danger."
+                    )
+                    .releaseDate(1972)
+                    .director("Francis Ford Coppola")
+                    .actors(
+                            new ArrayList<>(List.of(
+                                    Actor.builder().name("Al Pacino").build(),
+                                    Actor.builder().name("Marlon Brando").build(),
+                                    Actor.builder().name("James Caan").build()
+                            ))
+                    ).build();
 
             repository.saveAll(
                     List.of(movie1, movie2)
