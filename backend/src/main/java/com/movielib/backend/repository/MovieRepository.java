503b017461836617f67553c956585da53e4fabc6
MEZIANE reda <reda.meziane@etu.univ-paris-diderot.fr>
2023-11-03 00:18:26 +0100

    Added Repository Interface for Movie, User, Review Entity

--- backend/src/main/java/com/movielib/backend/repository/MovieRepository.java
+++ backend/src/main/java/com/movielib/backend/repository/MovieRepository.java
@@ -4,10 +4,18 @@ import com.movielib.backend.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+    Optional<Movie> findMovieByTitle(String title);
+
     Optional<Movie> findMovieByTitleAndDirector(String title, String director);
+
+    List<Movie> findAllByOrderByReleaseDateDesc();
+
+    List<Movie> findAllByOrderByRatingDesc();
+
 }
---
+++ backend/src/main/java/com/movielib/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.movielib.backend.repository;
+
+import com.movielib.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
